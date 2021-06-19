03acb9d7f92d209ad44c032b618da47766aa0613
Yuya Kasai <moyoyomiyazawa@gmail.com>
2021-06-19 18:25:56 +0900

    トーストメッセージ表示機能を追加

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,11 @@ import axios from 'axios';
 import { useCallback, useState } from 'react';
 import { useHistory } from 'react-router';
 import { User } from '../types/api/user';
+import { useMessage } from '../hooks/useMessage';
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
   const [loading, setLoading] = useState(false);
   const login = useCallback(
     (id: string) => {
@@ -14,13 +16,19 @@ export const useAuth = () => {
       axios
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
-          console.log(res.data);
-          res.data ? history.push('/home') : alert('ユーザーが見つかりません');
+          if (res.data) {
+            showMessage({ title: 'ログインしました', status: 'success' });
+            history.push('/home');
+          } else {
+            showMessage({ title: 'ユーザーが見つかりません', status: 'error' });
+          }
         })
-        .catch(() => alert('ログインできません'))
+        .catch(() =>
+          showMessage({ title: 'ログインできません', status: 'error' })
+        )
         .finally(() => setLoading(false));
     },
-    [history]
+    [history, showMessage]
   );
   return { login, loading };
 };
